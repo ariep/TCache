78d307ef2a243428948e5bc39c2f6b20288c8fd5
Arie Peterson <ariep@xs4all.nl>
2015-05-10 14:53:18 +0200

    Change ID to be a Data.Text.

--- Data/TCache/ID.hs
+++ Data/TCache/ID.hs
@@ -5,7 +5,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Data.TCache.ID
   (
-    ID
+    ID(ID)
   , WithID(WithID)
   , new
   , Ref
@@ -19,32 +19,45 @@ module Data.TCache.ID
   , update
   ) where
 
-import           Control.Applicative ((<$>))
 import qualified Data.TCache      as T
 import qualified Data.TCache.Defs as T
 import qualified Data.TCache.IndexQuery as T
+
+import           Control.Applicative ((<$>))
+import           Data.Text           (Text,pack)
 import           Data.Traversable    (for)
 import           Data.Typeable       (Typeable)
 import           GHC.Generics        (Generic)
 import           Prelude hiding (lookup)
+import           System.Random       (newStdGen,randomRs)
 
 
-type ID
-  = Int
+newtype ID a
+  = ID Text
+  deriving (Eq,Ord,Generic,Typeable,Show,Read)
 
 data WithID a
   = WithID
     {
-      _id :: ID
+      _id :: ID a
     , object :: a
     }
   deriving (Generic,Typeable,Show)
 
 new :: forall a. (T.IResource (WithID a),T.PersistIndex (WithID a),Typeable a)
   => a -> T.STM (WithID a)
-new x = flip WithID x . succ . maybe 0 fst <$> T.maximumQ (selector :: WithID a -> ID)
+new x = loop where
+  loop = do
+    i <- ID <$> T.unsafeIOToSTM (randomText 8)
+    existing <- (selector :: WithID a -> ID a) T..==. i
+    case existing of
+      [] -> return $ WithID i x
+      _  -> loop
 
-selector :: WithID a -> ID
+randomText :: Int -> IO Text
+randomText l = pack . take l . randomRs ('a','z') <$> newStdGen
+
+selector :: WithID a -> ID a
 selector (WithID i _) = i
 
 newRef :: (T.IResource (WithID a),T.PersistIndex (WithID a),Typeable a)
@@ -55,7 +68,7 @@ type Ref a
   = T.DBRef (WithID a)
 
 lookup :: forall a. (T.IResource (WithID a),Typeable a)
-  => ID -> Ref a
+  => ID a -> Ref a
 lookup i = T.getDBRef $ T.keyResource (WithID i undefined :: WithID a)
 
 deref :: (T.IResource (WithID a),Typeable a)
@@ -72,7 +85,7 @@ maybeDeref r = T.readDBRef r >>= \case
 listWithID :: forall a. (T.IResource (WithID a),T.PersistIndex (WithID a),Typeable a)
   => T.STM [WithID a]
 listWithID = do
-  ids <- T.indexOf (selector :: WithID a -> ID)
+  ids <- T.indexOf (selector :: WithID a -> ID a)
   (concat <$>) . for ids $ \ (i,refs) -> case refs of
     [r] -> (: []) . WithID i <$> deref r
     []  -> return []
